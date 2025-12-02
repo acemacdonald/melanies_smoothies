001fa945a053c577fb4f9a25acaf22fa9513fa5d
Angus Macdonald <ace.macdonald@gmail.com>
2025-12-02 16:28:25 +0000

    Update streamlit_app.py
    Adding smoothiefruit nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,10 @@ if ingredients_list:
         
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
-
+# New section to display smoothiefruit nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 
 
